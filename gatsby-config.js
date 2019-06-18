fa3c6b5a39f4f078fca88c15214a349396186e35
Daniel Meilleur <daniel.meilleur@gmail.com>
2019-06-18 02:07:22 -0700

    add member page skeleton

--- gatsby-config.js
+++ gatsby-config.js
@@ -86,7 +86,11 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
+        whitelist: [
+          'main-container', 'img-container', 'img-container img', 'article-container',
+          'content-container', 'content-container p', 'iframe-container',
+          'iframe-container iframe'
+        ], // Don't remove this selector
         // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       }
---
+++ src/pages/members.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+// TODO: create  a login form to access data on this page
+
+const members = () => {
+  return (
+    <div>
+      Hello
+    </div>
+  )
+}
+
+// members.propTypes = {
+
+// }
+
+export default members
