7cd5ea8c41e265eacadcab7275b70bc18cb8bbc1
Daniel Meilleur <daniel.meilleur@gmail.com>
2019-07-26 18:09:52 -0700

    created concert component for concerts page

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import Layout from '../components/layout'
 import { graphql, useStaticQuery } from 'gatsby'
-// import ConcertCard from '../components/concertCard'
-// import {  BLOCKS } from '@contentful/rich-text-types';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
-// import { FaSignLanguage } from 'react-icons/fa';
-import Img from 'gatsby-image'
-import '../styles/style.scss'
+import Concert from '../components/concert'
 
 const ConcertsPage = () => {
   const concerts = useStaticQuery(graphql`
@@ -21,6 +16,7 @@ const ConcertsPage = () => {
               artisticDirection
               pianiste
               participation
+              summary { summary }
               description { json }
               poster {
                 title
@@ -39,24 +35,6 @@ const ConcertsPage = () => {
         } 
     }
   `)
-  
-  // const H4 = ({children}) => (
-  //   <h4 className="has-background-warning">{children}</h4>
-  // )
-  // const options = {
-  //   displayName: 'mesOptions',
-  //   renderNode: {
-  //     // eslint-disable-next-line react/display-name
-  //     // 'embedded-asset-block': (node) => {
-  //     //   const alt = node.data.target.fields.title['en-US']
-  //     //   const url = node.data.target.fields.file['en-US'].url
-  //     //   return <img alt={alt} src={url} />
-  //     // },
-  //     [BLOCKS.HEADING_4]: (node, children) => <H4>{children}</H4>
-  //   },
-  //   renderMark: {
-  //   }
-  // }
 
   return (
     <Layout>
@@ -67,45 +45,13 @@ const ConcertsPage = () => {
         className="is-divider"
         data-content="Nos prochain concert">
       </div>
-      {concerts.allContentfulConcerts.edges
-        .filter((edge) => edge.node.node_locale === 'en-US')
-        .map((edge, idx) => {
-          return (
-            <article className="section" key={idx}>
-              {/* <a name={edge.node.slug}></a> */}
-              <h2 className="title">{edge.node.concertName}</h2>
-              <h3 className="subtitle">{edge.node.subtitle}</h3>
-              <div className="columns">
-                <div className="column is-one-third" style={{ maxWidth: '400px' }}>
-                  <Img fluid={edge.node.poster.fluid} alt={edge.node.poster.description} />
-                </div>
-                <div className="column">
-                  <p>{edge.node.concertDate}</p>
-                  <p>Artistic Direction: {edge.node.artisticDirection}</p>
-                  <p>Pianiste: {edge.node.pianiste}</p>
-                  <p>{edge.node.subtitle}</p>
-                  {edge.node.participation && edge.node.participation.map((p, idx) => {
-                    return (
-                      <p key={idx}>{p}</p>
-                    )
-                  })}
-                </div>
-              </div>
-              <div className="content">
-                {documentToReactComponents(
-                  // edge.node.description.json, options
-                  edge.node.description.json
-                )}
-              </div>
-              {(
-                new Date(edge.node.concertDate) >= new Date()
-              ) &&
-                <p>achetez billets</p>
-              }
-              <hr />
-            </article>
-          )
-        })}
+      {
+        concerts.allContentfulConcerts.edges
+          .filter((edge) => edge.node.node_locale === 'en-US')
+          .map((edge, idx) => {
+            return (<Concert key={idx} {...edge.node} />)
+          })
+      }
     </Layout>
   )
 }
