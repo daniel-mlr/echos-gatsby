3b4678128d3fe3a992bbeccc6a0ff83efd59954a
Daniel Meilleur <daniel.meilleur@gmail.com>
2019-07-29 19:35:45 -0700

    issue-62 done

--- src/pages/members.js
+++ src/pages/members.js
@@ -1,13 +1,46 @@
 import React from 'react'
+import Layout from '../components/layout'
+import { Link, graphql, useStaticQuery } from 'gatsby'
 // import PropTypes from 'prop-types'
 
 // TODO: create  a login form to access data on this page
 
-const members = () => {
+const Members = ({pageContext: { locale }}) => {
+  const data = useStaticQuery(graphql`
+    query {
+      allContentfulMembres (
+        filter : {node_locale: {eq: "en-US"}}
+      )
+        {
+        edges {
+          node {
+            titre
+            updatedAt(formatString: "MMM Do, YYYY")
+            slug
+          }
+        }
+      }
+    }`
+  )
+
   return (
-    <div>
-      Hello
-    </div>
+    <Layout path="/members" locale={locale}>
+      <section className="section">
+        <ul>
+          {
+            data.allContentfulMembres.edges.map((edge, id) => {
+              return (
+                <li key={id}>
+                  <Link to={`/members/${edge.node.slug}`} >
+                    {edge.node.titre}
+                  </Link>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </section>
+    </Layout>
   )
 }
 
@@ -15,4 +48,4 @@ const members = () => {
 
 // }
 
-export default members
+export default Members
