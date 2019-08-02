2b81cbad61868fb1c1a0907c783b774e74f2ea73
Angel Meza <undead.85@gmail.com>
2019-08-01 18:35:23 -0700

    Add bilingual
    - To read buttons
    - To Concert page
    - To Blog page
    - To Concert grid card component
    
    Close #65

--- src/components/concertGridCard.js
+++ src/components/concertGridCard.js
@@ -1,57 +1,16 @@
 import React from 'react'
-// import localStyle from './blogGridCard.module.scss'
-import { graphql, useStaticQuery } from 'gatsby'
+
 import ConcertCard from '../components/concertCard'
 
-const ConcertGridCard = ({title}) => {
-  const concertData = useStaticQuery(graphql`
-    query {
-      allContentfulConcerts (sort: {fields: concertDate, order: ASC}) {
-        edges {
-          node {
-            concertName
-            concertDateFormated: concertDate (formatString: "MMMM Do, YYYY")
-            concertDate
-            artisticDirection
-            pianiste
-            participation
-            summary { summary }
-            poster { 
-              title 
-              description
-             # fixed(width: 300, height: 300) {
-             #   ...GatsbyContentfulFixed
-              fluid {
-                ...GatsbyContentfulFluid
-              }
-            }
-            prix
-            slug
-            ticketsUrl
-            node_locale
-          }
-        }
-      }
-    }
-  `)
-
-  // console.log('@@@@@', concertData.allContentfulConcerts.edges)
+const ConcertGridCard = ({title, buttonText, data}) => {
+  console.log('data',data)
   return (
     <section className="section">
       <div
         className="is-divider"
         data-content={title}>
       </div>
-      {
-        concertData.allContentfulConcerts.edges
-          .filter((edge) => edge.node.node_locale === 'en-US')
-          .filter((edge) => new Date(edge.node.concertDate) >= new Date())
-          .map((edge, idx) => {
-            return (
-              <ConcertCard node={edge.node} key={idx} />
-            )
-          })
-      }
+      <ConcertCard buttonText={buttonText} node={data.edges[0].node} />
     </section>
   )
 }
