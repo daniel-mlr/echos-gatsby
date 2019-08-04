1dddb9f25c7e9e328a827f09eb1262dc97a6dbc9
Daniel Meilleur <daniel.meilleur@gmail.com>
2019-08-04 00:41:26 -0700

    done issue-66

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,37 +1,19 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 import Layout from '../components/layout'
-// import PropTypes from 'prop-types'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Img from 'gatsby-image'
 import Head from '../components/head'
-// import blogStyles from './blog.module.scss'
 import Hero from '../components/hero'
 
-export const query = graphql`
-  query ($slug: String!) {
-    contentfulBlogues(slug: {eq: $slug}) {
-      titre
-      publicationDate(formatString: "MMMM Do, YYYY")
-      slug
-      body { json }
-    }
-    file(name: {eq: "noel_tricities-1920x592"}) {
-      childImageSharp {
-        fluid(quality: 90, maxWidth: 1366) {
-          ...GatsbyImageSharpFluid_withWebp
-        }
-      }
-    }
-  }
-`
 const Blog = ({pageContext: { locale, langtag }, data}) => {
+  console.log('@@@@', {langtag}, data)
   const options = {
     renderNode: {
       // eslint-disable-next-line react/display-name
       'embedded-asset-block': (node) => {
         const alt = node.data.target.fields.title['en-US']
         const url = node.data.target.fields.file['en-US'].url
-        // console.log('node',node)
         return <img alt={alt} src={url}/>
       },
     }
@@ -50,10 +32,21 @@ const Blog = ({pageContext: { locale, langtag }, data}) => {
           {/* <h1>Nouvelles</h1> */}
           <div className={''} >
             <article className={''} >
-              <header>
+              <header className="content">
                 <h2>{data.contentfulBlogues.titre}</h2>
-                <p className="is-italic">{data.contentfulBlogues.publicationDate}</p>
+                <p className="is-italic">
+                  {data.contentfulBlogues.publicationDate}
+                </p>
               </header>
+              <div 
+                className="content is-pulled-left"
+                style={{margin: '0 1rem 1rem 0'}}
+              >
+                <Img 
+                  fixed={data.contentfulBlogues.previewPicture.fixed}
+                  alt={data.contentfulBlogues.previewPicture.description}
+                />
+              </div>
               {documentToReactComponents(data.contentfulBlogues.body.json, options)}
               <div className="tags">
                 <Link className="tag is-primary" to={'/blog'}>Liste des nouvelles</Link>
@@ -68,4 +61,35 @@ const Blog = ({pageContext: { locale, langtag }, data}) => {
   )
 }
 
+export const query = graphql`
+  query ( 
+    $slug: String!
+    $langtag: String!  
+  ) 
+  { 
+    contentfulBlogues(
+      slug: {eq: $slug}
+      node_locale: {eq: $langtag}
+    ) {
+      titre
+      publicationDate(formatString: "MMMM Do, YYYY")
+      slug
+      body { json }
+      previewPicture {
+        title
+        description
+        fixed(width: 96, height:96) {
+          ...GatsbyContentfulFixed
+        }
+      }
+    }
+    file(name: {eq: "noel_tricities-1920x592"}) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 1366) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`
 export default Blog
