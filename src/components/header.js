717ad8f98a4c9bc5ba959eb0df4b88650ea5a878
daniel-mlr <daniel.meilleur@gmail.com>
2019-08-19 18:19:05 -0700

    larger logo for high res

--- src/components/header.js
+++ src/components/header.js
@@ -16,9 +16,9 @@ const Header = ({path, langtag}) => {
           // className={headerStyles.logo}
           className="logo"
           // src='https://res.cloudinary.com/danielmeilleurimg/image/upload/v1562662608/echos/Les_Echos_du_Pacifique_logo_color_1200x800-13.png'
-          src='https://res.cloudinary.com/danielmeilleurimg/image/upload/c_scale,w_200/v1562662608/echos/Les_Echos_du_Pacifique_logo_color_1200x800-13.png'
+          // src='https://res.cloudinary.com/danielmeilleurimg/image/upload/c_scale,w_200/v1562662608/echos/Les_Echos_du_Pacifique_logo_color_1200x800-13.png'
           // src='https://res.cloudinary.com/danielmeilleurimg/image/upload/v1565391515/echos/logo/logo_final_437x200.png'
-          // src='https://res.cloudinary.com/danielmeilleurimg/image/upload/c_scale,w_400/v1565391864/echos/logo/Les_Echos_du_Pacifique_logo_color_1200x800-13.png'
+          src='https://res.cloudinary.com/danielmeilleurimg/image/upload/c_scale,w_400/v1565391864/echos/logo/Les_Echos_du_Pacifique_logo_color_1200x800-13.png'
           alt='logo'
         />
       </LocalizedLink>
