d7562f837172995aa39c2a2275c9a76f30886774
daniel-mlr <daniel.meilleur@gmail.com>
2019-10-03 15:27:35 -0700

    react-responsive-carousel

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,43 +1,81 @@
 import React from 'react'
 import { graphql, useStaticQuery } from 'gatsby'
 import Img from 'gatsby-image'
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import {Carousel} from 'react-responsive-carousel'
 
 const Jumbotron = () => {
+  
   const data = useStaticQuery(graphql`
     query {
       site {
         siteMetadata {
-          hero { main, sub}
+          hero {
+            main
+            sub
+          }
         }
       }
-      # file(name: {eq: "nom_femme-crop-1920x1200"}) {
-      file(name: {eq: "Echo_Choir_1920x1200_flop"}) {
-      # file(name: {eq: "Marla_1920x1200"}) {
-        childImageSharp {
-          fluid(quality: 90, maxWidth: 1920) {
-            ...GatsbyImageSharpFluid_withWebp
+      allFile(
+        filter: {
+          relativeDirectory: { eq: "images/carousel" }
+        }
+      ) {
+        edges {
+          node {
+            id
+            name
+            childImageSharp {
+              fluid(quality: 90, maxWidth: 1920) {
+                ...GatsbyImageSharpFluid_withWebp
+              }
+            }
           }
         }
       }
     }
   `)
 
-  return (
+  const carouselOptions = {
+    showThumbs: false,
+    showStatus: false,
+    infiniteLoop: true,
+    autoPlay: true,
+    stopOnHover: false,
+    interval: 8000,
+    transitionTime: 900,
+    emulateTouch: false,
+  }
 
+  return (
     <section className="hero is-full-height has-background">
-      <Img
-        fluid={data.file.childImageSharp.fluid}
-        className="hero-background"
-        imgStyle={{
-          objectPosition: 'top',
-        }}
-      />
+      
+      <Carousel {...carouselOptions}>
+        {data.allFile.edges.map(edge => (
+          <Img
+            key={edge.node.id}
+            fluid={edge.node.childImageSharp.fluid}
+            className="hero-background"
+            imgStyle={{
+              objectPosition: "top",
+            }}
+          />
+        ))}
+      </Carousel>
+    
       <div className="hero-body">
         <div className="container semi-transparent-light">
-          <h1 className="site-name">LES ÉCHOS<br /><span id="site-name2">DU PACIFIQUE</span></h1>
+          <h1 className="site-name">
+            LES ÉCHOS
+            <br />
+            <span id="site-name2">DU PACIFIQUE</span>
+          </h1>
           <h2 className="subtitle site-subname">
-            La chorale francophone <br/>
-            du <span className="site-vancouver">Grand Vancouver</span>
+            La chorale francophone <br />
+            du{" "}
+            <span className="site-vancouver">
+              Grand Vancouver
+            </span>
           </h2>
         </div>
       </div>
