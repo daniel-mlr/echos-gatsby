a549b7e14747460f085ff922057788cfbf29af94
daniel-mlr <daniel.meilleur@gmail.com>
2019-10-06 15:57:53 -0700

    disabled purge-css

--- gatsby-config.js
+++ gatsby-config.js
@@ -139,25 +139,25 @@ module.exports = {
         ]
       }
     },
-    {
-      resolve: 'gatsby-plugin-purgecss',
-      options: {
-        printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        whitelist: [
-          'main-container', 'img-container', 'img-container img', 'article-container',
-          'content-container', 'content-container p', 'iframe-container',
-          'iframe-container iframe'
-        ], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        ignore: [ 
-          'style.scss',
-          'langSwitcher.module.scss',
-          'react-responsive-carousel/lib/styles/carousel.min.css'
-        ], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      }
-    }
+    // {
+    //   resolve: 'gatsby-plugin-purgecss',
+    //   options: {
+    //     printRejected: true, // Print removed selectors and processed file names
+    //     // develop: true, // Enable while using `gatsby develop`
+    //     // tailwind: true, // Enable tailwindcss support
+    //     whitelist: [
+    //       'main-container', 'img-container', 'img-container img', 'article-container',
+    //       'content-container', 'content-container p', 'iframe-container',
+    //       'iframe-container iframe'
+    //     ], // Don't remove this selector
+    //     // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
+    //     ignore: [ 
+    //       'style.scss',
+    //       'langSwitcher.module.scss',
+    //       'react-responsive-carousel/lib/styles/carousel.min.css'
+    //     ], // Ignore files/folders
+    //     // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+    //   }
+    // }
   ]
 }
