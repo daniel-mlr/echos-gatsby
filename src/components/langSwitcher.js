d02f7afcdf25ebeca2a77566991e1eb3502a96f4
daniel-mlr <daniel.meilleur@gmail.com>
2019-10-23 16:09:18 -0700

    replace <Head> with seo / helmet noindex (members)

--- src/components/langSwitcher.js
+++ src/components/langSwitcher.js
@@ -10,19 +10,18 @@ const LangSwitcher = ({path, langtag}) => {
     <span>
       {
         Object.keys(locales)
-        .filter(key => locales[key].locale !== langtag)
-        .map(key => (
-          <span key={locales[key].locale}>
-            <Link
-              to={locales[key].default ? path : `/${locales[key].path}${path}`}
-              className={localStyles.lang}
-            >
-              {locales[key].label}
-            </Link>
-          </span>
-        ))
+          .filter(key => locales[key].locale !== langtag)
+          .map(key => (
+            <span key={locales[key].locale}>
+              <Link
+                to={locales[key].default ? path : `/${locales[key].path}${path}`}
+                className={localStyles.lang}
+              >
+                {locales[key].label}
+              </Link>
+            </span>
+          ))
       }
-
     </span>
 
   )
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Layout from '../components/layout'
-import Head from '../components/head'
+// import Head from '../components/head'
+import SEO from '../components/seo'
 import labels from '../constants/misc'
 
 const NotFound = ({pageContext: { locale, langtag }}) => {
   console.log('langtag:', langtag)
   return (
     <Layout path="/" locale={locale} langtag={langtag}>
-      <Head title="404"/>
+      {/* <Head title="404"/> */}
+      <SEO title="404: Not Found"/>
       <article className="message is-danger">
         <div className="message-header">{labels['pageNotFound'][langtag]}</div>
         <div className="message-body">
